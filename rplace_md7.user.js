4ee4fee7321c5a8296207f8c5f5410c626f58bca
Lesh <11671272+derLesh@users.noreply.github.com>
2023-07-22 14:15:12 +0200

    Cloned r/placeDEs version

--- rplace_md7.user.js
+++ rplace_md7.user.js
@@ -1,7 +1,7 @@
 // ==UserScript== 
 // @name         r/place Template - MD7 Template Edition 
 // @namespace    http://tampermonkey.net/ 
-// @version      1.3 
+// @version      1.3.1 
 // @description  try to take over the canvas! Thanks to the devs from placeDE :heart: 
 // @author       Froked from placeDE Devs
 // @match        https://garlic-bread.reddit.com/embed* 
@@ -11,17 +11,13 @@
 // ==/UserScript==
 
 var overlayImage = null;
-
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-        console.log("asgfasgsdahdfashgdfhgdfsg", document.getElementsByTagName("garlic-bread-embed"));
-        document.getElementsByTagName("garlic-bread-embed")[0].shadowRoot.children[0].children[0].getElementsByTagName("garlic-bread-camera")[0].children[0].shadowRoot.children[0].appendChild(
-            (function () {
-                overlayImage = document.createElement("img");
-                updateImage();
-                overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1500px;height: 1000px;pointerEvents: 'none';`;
-                return i;
-            })())
+        const canvasContainer = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("div.layout").querySelector("garlic-bread-canvas").shadowRoot.querySelector("div.container");
+        overlayImage = document.createElement("img");
+        updateImage();
+        overlayImage.style = `position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1500px;height: 1000px;pointerEvents: 'none';`;
+        canvasContainer.appendChild(overlayImage);
     }, false);
 }
 
